f34b20320fc234112af94b8b6f1bccd9eab48cc1
Tom Johnston <tom.johnston.2017@live.rhul.ac.uk>
2024-12-04 23:42:25 +0000

    adding up differences

--- Day1/historian_hysteria.py
+++ Day1/historian_hysteria.py
@@ -3,10 +3,9 @@ def load_input_file(filename):
 
     with open(filename, newline='\n') as input_file:
         for line in input_file:
-            line = line.strip()
-            line = line.split('   ')
-            left_list.append(line[0])
-            right_list.append(line[1])
+            line = line.strip().split('   ')
+            left_list.append(int(line[0]))
+            right_list.append(int(line[1]))
 
     left_list.sort()
     right_list.sort()
@@ -14,5 +13,16 @@ def load_input_file(filename):
     return left_list, right_list
 
 
+def add_up_difference_between_two_numbers(left_list, right_list):
+    list_of_differences = []
+
+    for i in range(0, len(left_list)-1):
+        list_of_differences.append(abs(left_list[i] - right_list[i]))
+
+    return sum(list_of_differences)
+
+
 if __name__ == '__main__':
-    output = load_input_file('input.txt')
+    list_1, list_2 = load_input_file('input.txt')
+    answer = add_up_difference_between_two_numbers(list_1, list_2)
+    print(answer)
